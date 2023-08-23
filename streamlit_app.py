c955ea934d7c8b562c0504d76388b51f0b3f3973
Harshit Chauhan <harshitc95@gmail.com>
2023-08-24 01:25:07 +0530

    Update streamlit_app.py
    Added functionality to get new fruits from user and store it in snowflake table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,18 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
-#don't run anything below until troubleshooting is in-progress
-streamlit.stop()
-
-fruit_name = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding ', fruit_name)
-
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('from strreamlit')")
+#Allow the user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into FRUIT_LOAD_LIST values ('"+new_fruit+"')")
+    return "Thanks for adding " + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
+  streamlit.text(back_from_function)
