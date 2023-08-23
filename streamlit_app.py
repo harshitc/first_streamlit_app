21fee59f9ffb7b0020336cebe6178162d961f12d
Harshit Chauhan <harshitc95@gmail.com>
2023-08-23 16:40:18 +0530

    Update streamlit_app.py
    Added functionality for user to be able to enter a fruit name and get the details about it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#Fetching fruityvice data via api call
+#New section to display Fruityvice data via api
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+#Fetching fruityvice data via api call
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Below will only show the raw data in json form
 #streamlit.text(fruityvice_response.json())
